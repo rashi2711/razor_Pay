dc2a1fe920921bd3ec5e9cdcf02b8206631497e8
rashi2711 <rashigarg803@gmail.com>
2024-10-05 12:31:31 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Adjust this line to include your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
